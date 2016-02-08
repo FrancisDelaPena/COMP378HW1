ba71f60996f64d2b22f900059b61d48848f569a0
TONO-SAMA <jdelapena@luc.edu>
2016-02-08 11:39:39 -0600

    Final submission

--- Power.java
+++ Power.java
@@ -1,47 +1,49 @@
-public static double power(double x, int n){
-    if (n == 0){
-        return 1;
+class Power {
+    public static double power(double x, int n){
+        if (n == 0){
+            return 1;
+        }
+        if (n == 1){
+            return x;
+        }
+        if (n % 2 == 0){
+            double half = power(x, n/2);
+            return half * half;
+        }
+        else{
+            double half = power(x, (n -1)/2);
+            return x * half * half;
+        }
     }
-    if (n == 1){
-        return x;
-    }
-    if (n % 2 == 0){
-        double half = power(x, n/2);
-        return half * half;
-    }
-    else{
-        double half = power(x, (n -1)/2);
-        return x * half * half;
-    }
-}
 
-public static void main(String [] args){
-    double x = 2;
-    int n = 0;
+    public static void main(String [] args){
+        double x = 2;
+        int n = 0;
     
-    System.out.println("x = 2");
+        System.out.println("x = 2");
     
-    double answer = power(x, n);
-    System.out.println(answer);
+        double answer = power(x, n);
+        System.out.println(answer);
     
-    int n = 1;
+        n = 1;
     
-    System.out.println("n = 1");
+        System.out.println("n = 1");
     
-    answer = power(x, n);
-    System.out.println(answer);
+        answer = power(x, n);
+        System.out.println(answer);
     
-    int n = 2;
+        n = 2;
     
-    System.out.println("n = 2");
+        System.out.println("n = 2");
     
-    answer = power(x, n);
-    System.out.println(answer);
+        answer = power(x, n);
+        System.out.println(answer);
     
-    int n = 3;
+        n = 3;
     
-    System.out.println("n = 3");
+        System.out.println("n = 3");
     
-    answer = power(x, n);
-    System.out.println(answer);
+        answer = power(x, n);
+        System.out.println(answer);
+}
 }
